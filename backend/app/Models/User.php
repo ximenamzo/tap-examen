bb81307a24525e895dca7ec061eb31edf28dd579
ximenamzo <ximenamzo1994@gmail.com>
2026-07-03 15:21:21 -0600

    feat: instalacion de Sanctum y adaptacion de modelo User a MongoDB

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,14 +3,20 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+# use Illuminate\Database\Eloquent\Factories\HasFactory;
+# use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    # use HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +24,12 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'code',
         'name',
         'email',
         'password',
+        'phone',
+        'profile_photo',
     ];
 
     /**
@@ -33,16 +42,29 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
+    // Relación muchos a muchos con Perfiles
+    // (según lo ententido, un usuario puede tener varios perfiles)
+    // la reclutadora no constesto mis dudas
+    public function profiles()
+    {
+        return $this->belongsToMany(Profile::class, 'user_profile');
+    }
+
+
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
-    protected function casts(): array
+    /*protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
+    }*/
 }
